1461833b2d3f7f83b156ae64a563b2a78940dfbd
Amit Sharma <amit@neobrix.io>
2023-06-27 01:09:48 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
 import { MetaMaskSDK } from '@metamask/sdk';
+import { v4 as uuidv4 } from 'uuid';
+import Hex from 'crypto-js/enc-hex';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+
 
 
 
@@ -20,7 +24,7 @@ function App() {
   useEffect(() => {
     console.log('adding event listener', new Date().getSeconds());
     window.document.body.addEventListener('Singularity-mounted', () => {
-      let key = 5;
+      let key = 2;
 
       window.Singularity.init(key);
 
@@ -70,8 +74,70 @@ function App() {
   }
 
     const onPersonalMessageSignedClicked = async () => {
-        const signature = window.SingularityEvent.requestPersonalSignature(personalMessage, ethereum)
-        console.log('Signature',signature)
+        const signature = await window.SingularityEvent.requestPersonalSignatureCustomProvider(personalMessage, ethereum)
+        console.log('Signature demo',signature)
+        if (signature.metaData)
+            window.alert('Signature: ' + signature.metaData);
+    }
+
+
+    const onTypedMessageSignedClicked = async () => {
+        const domain = {
+            name: 'GamePay',
+            version: '1',
+            chainId: 97,
+            verifyingContract: '0xED975dB5192aB41713f0080E7306E08188e53E7f'
+        };
+
+        const types = {
+            bid: [
+                { name: 'bidder', type: 'address' },
+                { name: 'collectableId', type: 'uint256' },
+                { name: 'amount', type: 'uint256' },
+                { name: 'nounce', type: 'uint' }
+            ]
+        };
+
+        const message = {
+            bidder: '0xAa81f641d4b3546F05260F49DEc69Eb0314c47De',
+            collectableId: 1,
+            amount: 100,
+            nounce: 1
+        };
+
+        const primaryType ="bid";
+
+        const signature = await window.SingularityEvent.requestTypedSignatureCustomProvider(domain, primaryType, types, message, ethereum)
+
+        console.log('Signature demo',signature)
+        if (signature.metaData)
+            window.alert('Signature: ' + signature.metaData);
+    }
+
+    const handleReceiveTransactionClicked = async () => {
+
+        const clientReferenceId = uuidv4();
+
+        let body = {
+            clientReferenceId,
+            singularityTransactionType: 'RECEIVE',
+            transactionLabel: 'Label Here',
+            transactionDescription: 'Description',
+            transactionIconLink:
+                'https://singularity-icon-assets.s3.ap-south-1.amazonaws.com/currency/matic.svg',
+            clientReceiveObject: {
+                clientRequestedAssetId: 84,
+                clientRequestedAssetQuantity: 0.0001,
+            },
+        };
+
+        const secret =
+            'SSk49aq1/kQ1eKH7Sg+u4JsisvrycRcLopHdM6lNEMVe/p7lsSVoRiY0neFYNJkHoWVEK30bPAV2pNU2WwOJXQ==';
+
+        console.log('Body to generate signature ---->', body);
+        const requestString = JSON.stringify(body);
+        const signature = Hex.stringify(hmacSHA512(requestString, secret));
+        await window.SingularityEvent.transactionFlowCustomProvider(requestString, signature, ethereum)
     }
 
 
@@ -80,6 +146,8 @@ function App() {
       <p>{address}</p>
       <button onClick={onConnectMetamaskClicked}>{connectButtonLabel}</button>
       <button onClick={onPersonalMessageSignedClicked}>Get Personal Message Signed</button>
+      <button onClick={onTypedMessageSignedClicked}>Get Typed Message Signed</button>
+      <button onClick={handleReceiveTransactionClicked}>Start Receive Transaction</button>
     </div>
   );
 }
